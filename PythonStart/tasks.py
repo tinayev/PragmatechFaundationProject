dba280fe5baf51155895927de2c673120c2510ed
tinayev <ashraftinayev08@gmail.com>
2022-04-06 12:55:40 -0700

    ilk is

---
+++ PythonStart/tasks.py
@@ -0,0 +1,7 @@
+#This is a comment
+print("Hello, World!")
+print("true+1 kodun neticesi: true sozu duzgun yailmayib T boyuk herflerle olmali idi.")
+print("print(3>4 and 3+4/2) kodunn neticesi: False")
+print("print(type(int(float('3')))) kodun neticesi: <class 'int'>")
+print("3.14+int(3.14)) kodun neticesi: 6.140000000000001")
+print("False+True>int(False)) kodun neticesi: True")
