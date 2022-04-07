90e01f0d62718550bc60a923d0b453b9a255a092
tinayev <ashraftinayev08@gmail.com>
2022-04-07 08:38:05 -0700

    isin 2 ci hissesi

---
+++ PythonStart/game.py
@@ -0,0 +1,5 @@
+#This is a comment
+print("else və elif arasında fərq nədir?")
+print("mən if else istifadə edəndə niyə boşluq istifadə edirəm?")
+print("Elif- yada menasini verir.Eger yazdigim kodda if ve else true olmazsa onda bu menalarda istifade edirem")
+print("")
