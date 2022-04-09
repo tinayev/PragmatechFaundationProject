0c6208b5b1d3427c60c3a2158755de708f321bb0
tinayev <ashraftinayev08@gmail.com>
2022-04-09 11:21:38 -0700

    isin sonu

--- PythonStart/input.py
+++ PythonStart/input.py
@@ -1,1 +1,7 @@
-print("Yasinizi qeyd edin:")
+yas = int (input("yasinizi qeyd edin:"))
+if yas >= 16:
+    print("Imtahana giris ede bilersiniz ")
+    if yas < 16:
+        print("Imtahana giris etmeye yasiniz catmir ")
+        if yas >= 16:
+            prin
