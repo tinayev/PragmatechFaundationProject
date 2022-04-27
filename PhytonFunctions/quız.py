a10425a67ca01feffd3c67557e40c028d222c533
tinayev <ashraftinayev08@gmail.com>
2022-04-27 23:50:34 +0400

    isin yarisi

--- PhytonFunctions/quız.py
+++ PhytonFunctions/quız.py
@@ -1,14 +1,10 @@
-sual="""
-    1."Hansi mektebde oxuyursan?",
-        a)"239",
-        b)"272",
-        c)"10",
-        d)"90"
+quizf="""
+    
 """
 
-print(sual)
+print(quiz)
 cavab=input("Cavabinizi daxil edin(Sadece a,b,c ve d herflerini daxil ede biler) : ")
-if cavab=="b":
-    print("Tebrik edirik")
+if cavab==sual["duzgunCavab"]:
+    print("Tebrik edirik ")
 else:
     print("Cavabiniz sehvdir")
