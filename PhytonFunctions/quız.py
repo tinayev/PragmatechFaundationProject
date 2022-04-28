c21e5db4d858b65755b1dbeffc592cc4d06f58f3
tinayev <ashraftinayev08@gmail.com>
2022-04-29 00:32:03 +0400

    isin yarisi

--- PhytonFunctions/quız.py
+++ PhytonFunctions/quız.py
@@ -1,10 +1,46 @@
-quizf="""
-    
+sual="""
+1. Azerbaycan Respublikasinin prezidenti kimdir?
+    a)Ilham Eliyev
+    b)Heyder Eliyev
+    c)Elcibey
+    d)Ayaz Mutellimov
 """
 
-print(quiz)
-cavab=input("Cavabinizi daxil edin(Sadece a,b,c ve d herflerini daxil ede biler) : ")
-if cavab==sual["duzgunCavab"]:
-    print("Tebrik edirik ")
+print(sual)
+cavab=input("Cavabinizi daxil edin (Sadece a,b,c ve ya d ededi daxil edile biler) : ")
+if cavab== "a":
+    print("Tebrikler")
+else:
+    print("Cavabiniz sehvdir")
+    
+    
+sual="""
+2.Turkiyenin Paytaxti haradir?
+   a)Ankara
+   b)Istanbul
+   c)Izmir
+   d)Antalya
+"""
+
+print(sual)
+cavab=input("Cavabinizi daxil edin (Sadece a,b,c ve ya d ededi daxil edile biler) : ")
+if cavab== "a":
+    print("Tebrikler")
+else:
+    print("Cavabiniz sehvdir")
+    
+    
+sual="""
+3.Azerbaycanin paytaxti haradir?
+  a)Zaqatala
+  b)Gence
+  c)Baki
+  d)Samaxi
+"""
+
+print(sual)
+cavab=input("Cavabinizi daxil edin (Sadece a,b,c ve ya d ededi daxil edile biler) : ")
+if cavab== "c":
+    print("Tebrikler")
 else:
     print("Cavabiniz sehvdir")
