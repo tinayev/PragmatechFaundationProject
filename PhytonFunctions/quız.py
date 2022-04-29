886fd7a6676497c1cb913c09bf82899ce96f098f
tinayev <ashraftinayev08@gmail.com>
2022-04-30 00:47:40 +0400

    isin sonu

--- PhytonFunctions/quız.py
+++ PhytonFunctions/quız.py
@@ -44,3 +44,39 @@ if cavab== "c":
     print("Tebrikler")
 else:
     print("Cavabiniz sehvdir")
+    
+    
+sual="""
+4.Ispanyanin paytaxti haradir?
+a)Barselona
+b)Madrid
+c)Elce
+d)Malaqa
+"""
+
+
+print(sual)
+cavab=input("Cavabinizi daxil edin (Sadece a,b,c ve ya d ededi daxil edile biler) : ")
+if cavab== "b":
+    print("Tebrikler")
+else:
+    print("Cavabiniz sehvdir")
+    
+    
+sual="""
+5.Fransanin paytaxti haradir?
+a)Marsel
+b)Lyon
+c)Nant
+d)Paris
+"""
+
+
+print(sual)
+cavab=input("Cavabinizi daxil edin (Sadece a,b,c ve ya d ededi daxil edile biler) : ")
+if cavab== "d":
+    print("Tebrikler")
+else:
+    print("Cavabiniz sehvdir")
+    
+yas = int("Duz cavab sayi-5.Sehv cavab sayi-0. Umumi bal-50")
