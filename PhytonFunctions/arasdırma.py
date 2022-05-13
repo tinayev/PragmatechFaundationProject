2e5d6e0168eb76f2cffa8d963ea0e3454ce708da
tinayev <ashraftinayev08@gmail.com>
2022-05-13 23:36:27 +0400

    arasdirmanin sonu

--- PhytonFunctions/arasdırma.py
+++ PhytonFunctions/arasdırma.py
@@ -1,7 +1,7 @@
 PySide6 package üçün aşağıdakı Classların nə işə yaradığını və başlıca metodlarını qeyd edin
 QWidget-butun widgeler ucun en esas sinif kimi yeniden pencere yaradir
 QTimeEdit-
-QTabWidget-
+QTabWidget-her bır tab ıle elaqelı sehıfelerı gostermek ucun ıstıfade olunan "sehife sahesi" temin edir.
 QPushButton-istenilen GUI dizayninin emr duymesi ucun en vacib ve en cox istifade olunan idareetmedir.
 QMenu-Pulsuz virtuallasdirma programidir.virtuallasdirma programi emeliyyat sistemi daxilinde emeliyyat sistemini idare etmeye imkan veren texnologiyadir.
 QLabel-Qlabel mehsula mexsus meyarlara uygun olaraq yetisdirilen ve ya emal edilen mehsul ucun keyfiyyet etiketidir.
